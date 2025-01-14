57e694d7674f5fbe7f9e496f953b3c4b793278d7
maxkrcl <138573266+maxkrcl@users.noreply.github.com>
2025-01-14 11:24:15 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const tg = window.Telegram.WebApp;
 
 const userPhoto = document.querySelector('.user-photo > img');
-userPhoto.setAttribute('src', tg.initDataUnsafe.photo_url);
+userPhoto.setAttribute('src', tg.initDataUnsafe.user.photo_url);
 
 const userName = document.querySelector('.user-name');
-userName.textContent = tg.initDataUnsafe.first_name;
+userName.textContent = tg.initDataUnsafe.user.first_name;
