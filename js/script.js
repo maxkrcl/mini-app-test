d7edc0c27eb2dff702be836dc1fc3d125a031a76
maxkrcl <138573266+maxkrcl@users.noreply.github.com>
2025-01-15 09:27:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,35 @@
-const tg = window.Telegram.WebApp;
+const dayNamesRU = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота'
+];
 
-const userPhoto = document.querySelector('.user-photo > img');
-userPhoto.setAttribute('src', tg.initDataUnsafe.user.photo_url);
+const monthNamesRU = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь'
+];
 
-const userName = document.querySelector('.user-name');
-userName.textContent = tg.initDataUnsafe.user.first_name;
+const currentDate = new Date();  // объект даты
+
+const dayTitle = document.querySelector('.day-title');
+dayTitle.textContent = dayNamesRU[currentDate.getDay()];
+
+const dayDate = document.querySelector('.day-date');
+const day = currentDate.getDate().toString().padStart(2, '0');  // день
+const month = monthNamesRU[currentDate.getMonth()];             // месяц
+const year = currentDate.getFullYear();                         // год
+dayDate.textContent = `${day} ${month} ${year}`;
