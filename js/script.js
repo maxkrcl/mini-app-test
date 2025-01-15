49f087c70972be4c4d693a40616a0903ec9dec39
maxkrcl <138573266+maxkrcl@users.noreply.github.com>
2025-01-15 16:57:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const tg = window.Telegram.WebApp;
+
 const dayNamesRU = [
   'Воскресенье',
   'Понедельник',
@@ -33,3 +35,9 @@ const day = currentDate.getDate().toString().padStart(2, '0');  // день
 const month = monthNamesRU[currentDate.getMonth()];             // месяц
 const year = currentDate.getFullYear();                         // год
 dayDate.textContent = `${day} ${month} ${year}`;
+
+const userPhoto = document.querySelector('.user-photo > img');
+userPhoto.setAttribute('src', tg.initDataUnsafe.user.photo_url);
+
+const userName = document.querySelector('.user-name');
+userName.textContent = tg.initDataUnsafe.user.first_name;
